607fee7255d8c2dff34c205d0a621a68b3284fcd
Ben James <bmjames@gmail.com>
2014-05-03 13:46:00 +0100

    AutoScaling groups and ingress from security groups

--- Data/CfnGraph.hs
+++ Data/CfnGraph.hs
@@ -7,6 +7,7 @@ module Data.CfnGraph where
 
 import Control.Applicative
 
+import Data.ByteString  (ByteString)
 import Data.Map
 import Data.Reify
 import Data.Text        (Text)
@@ -16,18 +17,69 @@ import Data.Traversable (traverse)
 type Port = Int
 type Timeout = Int
 
-data Resource a where
+type Name = Text
 
-  SG  :: [Ingress] -> Resource SecurityGroup
+data Resource a where
+  Stack :: [Resource AutoScalingGroup] -> Resource Stack
+
+  ASG :: Capacity
+      -> [Tag]
+      -> Resource LaunchConfig
+      -> [Resource LoadBalancer]
+      -> Resource AutoScalingGroup
+
+  LC  :: KeyName
+      -> ImageId
+      -> UserData
+      -> [Resource SecurityGroup]
+      -> Resource LaunchConfig
+
+  Ing :: IpProtocol
+      -> Port
+      -> Port
+      -> Resource IngressSource
+      -> Resource Ingress
+
+  IngSrc :: ResIngressSource a -> Resource IngressSource
+
+  SG  :: [Resource Ingress] -> Resource SecurityGroup
 
   LB  :: [Listener]
       -> HealthCheck
       -> [Resource SecurityGroup]
       -> Resource LoadBalancer
 
+data ResIngressSource a where
+  ResIpSource :: CidrIp -> ResIngressSource CidrIp
+  ResSGSource :: Resource SecurityGroup -> ResIngressSource SecurityGroup
+
 data ResourceGraph a s where
 
-  GraphSG :: [Ingress] -> ResourceGraph SecurityGroup s
+  GraphSt :: [ResourceGraph AutoScalingGroup s]
+          -> ResourceGraph Stack s
+
+  GraphASG :: Capacity
+           -> [Tag]
+           -> ResourceGraph LaunchConfig s
+           -> [ResourceGraph LoadBalancer s]
+           -> ResourceGraph AutoScalingGroup s
+
+  GraphLC :: KeyName
+          -> ImageId
+          -> UserData
+          -> [ResourceGraph SecurityGroup s]
+          -> ResourceGraph LaunchConfig s
+
+  GraphIng :: IpProtocol
+           -> Port
+           -> Port
+           -> ResourceGraph IngressSource s
+           -> ResourceGraph Ingress s
+
+  GraphIngSrc :: IngressSourceGraph a s -> ResourceGraph a s
+
+  GraphSG :: [ResourceGraph Ingress s]
+          -> ResourceGraph SecurityGroup s
 
   GraphLB :: [Listener]
           -> HealthCheck
@@ -35,6 +87,11 @@ data ResourceGraph a s where
           -> ResourceGraph LoadBalancer s
 
   Ref :: ResourceType a -> s -> ResourceGraph a s
+
+data IngressSourceGraph a s where
+  GraphIpSrc :: CidrIp -> IngressSourceGraph IngressSource s
+  GraphSGSrc :: ResourceGraph SecurityGroup s
+             -> IngressSourceGraph IngressSource s
 
 data WrappedGraph s where
   Wrap :: ResourceType a -> ResourceGraph a s -> WrappedGraph s
@@ -47,25 +104,53 @@ instance MuRef (Resource a) where
               => (forall b. (MuRef b, WrappedGraph ~ DeRef b) => b -> f u)
               -> Resource a
               -> f (ResourceGraph a u)
-    mapDeRef' _ (SG igs) = pure $ GraphSG igs
+    mapDeRef' g (SG ings) =
+      GraphSG <$> traverse (fmap (Ref IngType) . g) ings
+    mapDeRef' g (Ing prot from to source) =
+      GraphIng prot from to <$> (Ref IngSrcType <$> g source)
+    mapDeRef' g (IngSrc src) = GraphIngSrc <$> case src of
+      ResIpSource cIp -> GraphIpSrc <$> pure cIp
+      ResSGSource sg  -> GraphSGSrc <$> (Ref SGType <$> g sg)
     mapDeRef' g (LB ls hc sgs) =
       GraphLB ls hc <$> traverse (fmap (Ref SGType) . g) sgs
+    mapDeRef' g (ASG cap tags lconf lbs) =
+      GraphASG cap tags <$> (Ref LCType <$> g lconf)
+                        <*> traverse (fmap (Ref LBType) . g) lbs
+    mapDeRef' g (LC key ami udata sgs) = 
+      GraphLC key ami udata <$> traverse (fmap (Ref SGType) . g) sgs
+    mapDeRef' g (Stack asgs) =
+      GraphSt <$> traverse (fmap (Ref ASGType) . g) asgs
 
 data ResourceType a where
-  LBType :: ResourceType LoadBalancer
-  SGType :: ResourceType SecurityGroup
+  ASGType :: ResourceType AutoScalingGroup
+  LCType  :: ResourceType LaunchConfig
+  LBType  :: ResourceType LoadBalancer
+  IngType :: ResourceType Ingress
+  IngSrcType :: ResourceType IngressSource
+  SGType  :: ResourceType SecurityGroup
+  StType  :: ResourceType Stack
 
 getResourceType :: Resource a -> ResourceType a
 getResourceType st = case st of
-  SG {} -> SGType
-  LB {} -> LBType
+  ASG    {} -> ASGType
+  LC     {} -> LCType
+  Ing    {} -> IngType
+  IngSrc {} -> IngSrcType
+  SG     {} -> SGType
+  LB     {} -> LBType
+  Stack  {} -> StType
 
 data Refl a b where Refl :: Refl a a
 
 typeEq :: ResourceType a -> ResourceType b -> Maybe (Refl a b)
-typeEq LBType LBType = Just Refl
-typeEq SGType SGType = Just Refl
-typeEq _      _      = Nothing
+typeEq LBType LBType   = Just Refl
+typeEq LCType LCType   = Just Refl
+typeEq IngType IngType = Just Refl
+typeEq IngSrcType IngSrcType = Just Refl
+typeEq SGType SGType   = Just Refl
+typeEq ASGType ASGType = Just Refl
+typeEq StType StType   = Just Refl
+typeEq _      _        = Nothing
 
 type RefName = Unique
 
@@ -76,20 +161,43 @@ getRef :: Map RefName (WrappedGraph RefName)
 getRef m t n = case m ! n of
   Wrap t' e -> (\Refl -> e) <$> typeEq t t'
 
-conv :: Resource a -> IO (Map RefName (WrappedGraph RefName), Maybe (ResourceGraph a RefName))
+conv :: Resource a
+     -> IO (Map RefName (WrappedGraph RefName), Maybe (ResourceGraph a RefName))
 conv e = do
   Graph l n <- reifyGraph e
   let m = fromList l
   return (m, getRef m (getResourceType e) n)
 
 --------------------------------------------------------------------------------
+data Stack = St [AutoScalingGroup]
+
+data AutoScalingGroup = AutoScalingGroup -- TODO
+
+data Capacity = Capacity Int Int Int
+                deriving Show
+
+data Tag = Tag Text Text Bool
+           deriving Show
+
+type KeyName = Text
+type InstanceType = Text
+type ImageId = Text
+type UserData = ByteString
+
+data LaunchConfig = LaunchConfig KeyName ImageId UserData [SecurityGroup]
+                    deriving Show
+
 data LoadBalancer = LoadBalancer [Listener] HealthCheck [SecurityGroup]
                     deriving Show
 
 data SecurityGroup = SecurityGroup [Ingress] deriving Show
 
-data Ingress = Ingress IpProtocol Port Port CidrIp
+data Ingress = Ingress IpProtocol Port Port IngressSource
                deriving Show
+
+data IngressSource = IpSource CidrIp
+                   | SGSource SecurityGroup
+                     deriving Show
 
 data IpProtocol = TCP | UDP deriving Show
 
@@ -108,6 +216,11 @@ data HealthCheck = HealthCheck Protocol Port FilePath Timeout
 instance Show (ResourceType a) where
   show SGType = "SecurityGroup"
   show LBType = "LoadBalancer"
+  show LCType = "LaunchConfig"
+  show ASGType = "AutoScalingGroup"
+  show IngType = "Ingress"
+  show IngSrcType = "IngressSource"
+  show StType = "Stack"
 
 instance Show (WrappedGraph RefName) where
   show (Wrap t a) = concat ["(", show a, " :: ", show t, ")"]
@@ -116,4 +229,16 @@ instance Show s => Show (ResourceGraph a s) where
   show (Ref t n)    = unwords ["(Ref", show t, show n] ++ ")"
   show (GraphSG is) = "(GraphSG " ++ show is ++ ")"
   show (GraphLB ls hc sgs) =
-    unwords ["(GraphLB", show ls, show hc, show sgs] ++ ")" 
+    unwords ["(GraphLB", show ls, show hc, show sgs] ++ ")"
+  show (GraphASG cap tags lconf lbs) =
+    unwords ["(GraphASG", show cap, show tags, show lconf, show lbs] ++ ")"
+  show (GraphSt asgs) = "(GraphStack " ++ show asgs ++ ")"
+  show (GraphLC key ami udata sgs) =
+    unwords ["(GraphLC", show key, show ami, show udata, show sgs] ++ ")"
+  show (GraphIng prot from to source) =
+    unwords ["(GraphIng", show prot, show from, show to, show source] ++ ")"
+  show (GraphIngSrc src) = "(GraphIngSrc " ++ show src ++ ")"
+
+instance Show s => Show (IngressSourceGraph a s) where
+  show (GraphIpSrc cidrIp) = "(GraphIpSrc " ++ show cidrIp ++ ")"
+  show (GraphSGSrc sg) = "(GraphSGSrc " ++ show sg ++ ")"
--- Main.hs
+++ Main.hs
@@ -3,17 +3,31 @@
 module Main where
 
 import Data.CfnGraph
+import Data.Map (toList)
 
 
-myStack :: Resource LoadBalancer
+myStack :: Resource Stack
 myStack =
-  LB [httpListener] healthCheck [sshSecGroup]
+  Stack [asg]
 
   where
+    asg          = ASG (Capacity 1 2 1) [] launchConf [loadBalancer]
+    launchConf   = LC "my-key" "ami-foobar" udata [sshSecGroup, appSecGroup]
+    udata        = ""
+    loadBalancer = LB [httpListener] healthCheck [lbSecGroup]
     httpListener = Listener 9000 80 HTTP
     healthCheck  = HealthCheck HTTP 9000 "/management/healthcheck" 5
-    sshSecGroup  = SG [httpIngress]
-    httpIngress  = Ingress TCP 80 9000 (CIp "77.91.248.0" 21)
+    
+    sshSecGroup  = SG [sshIngress]
+    lbSecGroup   = SG []
+    appSecGroup  = SG appIngress
+    
+    appIngress  = [ Ing TCP 9000 9000 (IngSrc ipSource)
+                  , Ing TCP 9000 9000 (IngSrc $ ResSGSource lbSecGroup)
+                  ]
+    sshIngress  = Ing TCP 22   22   (IngSrc ipSource)
+
+    ipSource = ResIpSource $ CIp "77.91.248.0" 21
 
 main :: IO ()
-main = conv myStack >>= print
+main = conv myStack >>= mapM_ print . toList . fst
